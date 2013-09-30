ac94c2f740601441b7b9d6fffd945227af043e5e
padminisitaraman <padmini.vittal@sella.it>
2013-09-30 16:30:04 +0530

    Take it
    Test it

---
+++ test1e10/About.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test1e10
+{
+    public partial class About : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Request["sid"] != null && Convert.ToString(Request["sid"]) != Session.SessionID)
+            {
+                Response.Cookies.Add(new HttpCookie("ASP.NET_SessionId", Convert.ToString(Request["sid"])));
+            }
+            if (Request["fid"] != null && Convert.ToString(Request["fid"]).Length > 0)
+            {
+                HttpCookie fidcookie = new HttpCookie("fid", Convert.ToString(Request["fid"]));
+                fidcookie.Secure = true;
+                Response.Cookies.Add(fidcookie);
+            }
+            if (Request.Cookies["cook"] != null)
+            {
+
+                HttpCookie cookie = new HttpCookie("cook", "nochicken");
+                cookie.Secure = true;
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                HttpCookie cookie = new HttpCookie("cook", "chicken");
+                cookie.Secure = true;
+                Response.Cookies.Add(cookie);
+                
+            }
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+}
---
+++ test1e10/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test1e10
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("cook", "chicken");
+            cookie.Secure = true;
+            Response.Cookies.Add(cookie);
+
+            if (((Request.UserAgent.ToLower().IndexOf("safari") >= 0 && Request.UserAgent.ToLower().IndexOf("chrome") < 0) || (Request.UserAgent.ToLower().IndexOf("windows") >= 0)) && Convert.ToString(Session["refirected"]).Length <= 0)
+            {
+                Session["refirected"] = true;
+                string indexpath = Request.Url.ToString();
+                indexpath = indexpath.ToUpper().Replace("DEFAULT.ASPX", "About.aspx");
+                Response.Write(string.Format("<script type=text/javascript>top.location.href='{0}?sid={1}&fid={2}'</script>", indexpath, Session.SessionID, "chicken"));
+                Response.End();
+            }
+            Response.Redirect("~/About.aspx");
+        }
+    }
+}
